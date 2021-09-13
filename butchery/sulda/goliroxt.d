01a739235a3ed37dc75dd4e39d2db0b244c66f50
GwendolyneFreddy <gwendolyne.freddy@gmail.com>
2021-09-13 10:44:38 +0200

    Added component #50 : 2 butcheries in Suldanessellar and Watcher's Keep (Carston and the machine) - Golems

---
+++ butchery/sulda/goliroxt.d
@@ -0,0 +1,6 @@
+BEGIN ~GOLIROXT~
+
+IF ~NumTimesTalkedTo(0)~ BEGIN newreplique
+  SAY @14664
+	IF ~~ THEN DO ~SetGlobal("D9beginbutchery","AR3017",1)~ EXIT
+END
---
+++ butchery/sulda/goliroxx.d
@@ -0,0 +1,6 @@
+BEGIN ~GOLIROXX~
+
+IF ~NumTimesTalkedTo(0)~ BEGIN newreplique
+  SAY @14664
+	IF ~~ THEN DO ~SetGlobal("D9beginbutchery","AR2800",1)~ EXIT
+END
